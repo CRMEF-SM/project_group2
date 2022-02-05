3f0c8694cc85fbe28f01a1214ff237396c4c7e8d
Hamza <hamzamoha123@gmail.com>
2022-02-05 22:31:59 +0100

    List all routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    $routes = $router->app->router->getRoutes();
+    return view("home")->with("routes", $routes);
 });
 
 // parents
