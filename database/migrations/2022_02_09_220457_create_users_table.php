8a85ac43ed089134c1e4b0ae2e8705565ce92f90
Hamza <hamzamoha123@gmail.com>
2022-02-10 13:18:41 +0100

    Auth done

---
+++ database/migrations/2022_02_09_220457_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email');
+            $table->string('password');
+            $table->timestamps();
+        });
+        $user = new User();
+        $user->nom = 'Smith';
+        $user->prenom = 'John';
+        $user->email = 'admin@test.com';
+        $user->password = Hash::make('password');
+        $user->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
